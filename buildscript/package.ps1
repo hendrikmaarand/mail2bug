3e97af51379e750d0e398491e70ebdbbf0236b4c
Hendrik Maarand <v-hemaar@microsoft.com>
2015-06-19 10:12:02 +0300

    E-Spec: AWS-109579: Create build configuration for Mail2Bug
    Buildscript

--- buildscript/package.ps1
+++ buildscript/package.ps1
@@ -2,8 +2,15 @@ $commonFile = $PSScriptRoot + "\common.ps1"
 . $commonFile
 
 $nugetDir = $($Env:TF_BUILD_BINARIESDIRECTORY + "\nuget")
+
+write-output "package.nuspec: $($PSScriptRoot + '\package.nuspec')"
+write-output "nugetdir: $nugetDir"
  
 Copy-Item $($PSScriptRoot + "\package.nuspec") $nugetDir 
 
+write-output "after copy"
+
 $newVersion = GetVersionByReason
 & 'c:\tools\nuget\nuget.exe' pack $($nugetDir + "\package.nuspec") -Version "$newVersion" -OutputDirectory $nugetDir
+
+write-output "end"
