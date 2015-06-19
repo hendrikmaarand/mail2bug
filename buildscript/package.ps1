b1d8458f8027c3e0881987fcbfb96ebacc62c3b0
Hendrik Maarand <v-hemaar@microsoft.com>
2015-06-19 11:08:40 +0300

    E-Spec: AWS-109579: Create build configuration for Mail2Bug
    Buildscript

--- buildscript/package.ps1
+++ buildscript/package.ps1
@@ -2,14 +2,17 @@ $commonFile = $PSScriptRoot + "\common.ps1"
 . $commonFile
 
 $nugetDir = $($Env:TF_BUILD_BINARIESDIRECTORY + "\nuget")
+$mail2BugDir = $($nugetDir + "\Mail2Bug")
 $binaryPath = $Env:TF_BUILD_BINARIESDIRECTORY
 
 New-Item -ItemType directory -Path $nugetDir
+New-Item -ItemType directory -Path $mail2BugDir
+
 Copy-Item $($PSScriptRoot + "\package.nuspec") $nugetDir 
-Copy-Item $($binaryPath + "\*.dll") $nugetDir
-Copy-Item $($binaryPath + "\*.config") $nugetDir
-Copy-Item $($binaryPath + "\Mail2Bug.exe") $nugetDir
-Copy-Item $($binaryPath + "\DpapiTool.exe") $nugetDir
+Copy-Item $($binaryPath + "\*.dll") $mail2BugDir
+Copy-Item $($binaryPath + "\*.config") $mail2BugDir
+Copy-Item $($binaryPath + "\Mail2Bug.exe") $mail2BugDir
+Copy-Item $($binaryPath + "\DpapiTool.exe") $mail2BugDir
 
 $newVersion = GetVersionByReason
 & 'c:\tools\nuget\nuget.exe' pack $($nugetDir + "\package.nuspec") -Version "$newVersion" -OutputDirectory $nugetDir
