3ce9de9473507f3161f636f2d0c5d1d18f2a1562
Hendrik Maarand <v-hemaar@microsoft.com>
2015-08-28 14:13:26 +0300

    E-Spec: 223208: Set up Mail2Bug scheduler to rebuild the configuration when necessary
    Added the script for building the configuration.

---
+++ Mail2Bug/Scripts/build-config.ps1
@@ -0,0 +1,56 @@
+param(
+    [string]$git = "git.exe",
+    [string]$configurationRepository = "https://vstfskype.europe.corp.microsoft.com/tfs/Skype/ES/_git/internal_tfs_mail2bug-config",
+    [string]$instance = "skype.txt",
+    [string]$generate = "generate.ps1",
+    [string]$generatedFolder = "Resources",
+    [string]$deployFolder = "/Users/v-hemaar/Desktop"
+)
+
+
+$tmpConfDirectory = "tmp-conf-directory"
+
+
+# remove old conf stuff
+if(Test-Path $tmpConfDirectory) {
+    write "delete $tmpConfDirectory"
+    rm -Recurse -Force $tmpConfDirectory
+}
+
+write "clone $configurationRepository"
+# clone fresh configuration source
+&$git clone $configurationRepository $tmpConfDirectory
+
+# build the configurations
+try {
+    Push-Location "$tmpConfDirectory"
+    write "build"
+    write ""
+    
+    $cmd = "$generate -configuration $instance"
+    iex "& $cmd"
+    write ""
+}
+catch {
+    write-error $_.Exception
+
+    # We could not build the configurations, exit the script so the existing in the deployment is not messed up
+    Exit
+}
+finally {
+    Pop-Location
+}
+
+# delete existing configuration"
+if(Test-Path "$deployFolder/$generatedFolder") {
+    write "delete deployed conf: $deployFolder/$generatedFolder"    
+    rm "$deployFolder/$generatedFolder"
+}
+
+write "copy"
+# copy the configurations 
+cp "$tmpConfDirectory/$generatedFolder" "$deployFolder"
+
+
+
+Write-Host "Done!"
