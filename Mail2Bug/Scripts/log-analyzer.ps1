2340c76b95b13145e3c615b4542e76f3ec3fe12b
Hendrik Maarand <v-hemaar@microsoft.com>
2015-09-16 09:33:01 +0300

    E-Spec: 254088: Mail2Bug monitoring service
    Script for detecting if there have been errors in the past n minutes or if it has done any processing at all.

---
+++ Mail2Bug/Scripts/log-analyzer.ps1
@@ -0,0 +1,86 @@
+param(
+    [string]$pathToLogs = ".\Logs",
+    [int]$minutesToLookBack = 5,
+    [int]$numLastLinesOfLog = 10000
+)
+
+$log_file_timestap = Get-Date -Format "yyyy-MM-dd"
+$timestamp = Get-Date
+$timestamp = $timestamp.AddMinutes(-$minutesToLookBack)
+$timestamp = Get-Date -Format "yyyy-MM-dd hh:mm" $timestamp
+
+
+$filename = "$pathToLogs\$log_file_timestap.log"
+
+$pattern = "^([0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}),[0-9]{3} \[[0-9]+\] (.*)"
+$stack_pattern = "^\s{3}at\s.*"
+
+$processing_pattern = "*Reading messages from inbox*"
+$error_pattern = "*ERROR*"
+
+
+$processing = $false
+$errors = $false
+
+$contents = Get-Content $filename -Tail $numLastLinesOfLog
+$count = $contents.Count
+
+# find the starting point of relevant logs
+for($i = 0; $i -le $count; $i++) {
+    if($contents[$i] -match $pattern) {
+        $datetime = $matches[1]
+        $date = Get-Date -Date $datetime
+
+        if($date -ge $timestamp) {
+            break
+        }
+    }
+} 
+
+# process the relevant logs
+for($i; $i -le $count; $i++) {
+    $line = $contents[$i]
+
+    if($line -match $pattern) {
+        $datetime = $matches[1]
+        $text = $matches[2]
+
+        $date = Get-Date -Date $datetime
+
+        if($date -ge $timestamp) {
+            if($text -like $processing_pattern) {
+                $processing = $true
+            }
+
+            if($text -like $error_pattern) {
+                $errors = $true
+                write $line
+            }            
+        }
+        else {
+            # log date should not be less than the timestamp; something odd here
+        }        
+    }
+	elseif(-not $line) {
+		# ignore
+	}
+    else {
+        # if the line does not match the pattern then it is probably stack trace or something
+        $errors = $true
+        if(-not ($line -match $stack_pattern)) {
+            write $line # do not write stack trace to output 
+        }        
+    }
+}
+
+if(-not $processing -and -not $errors) {
+    write "No log entries indicating that Mail2Bug is processing were found!"
+}
+
+if(-not $processing -or $errors) {
+    # not processing mails or having errors is a problem
+    exit 2
+} 
+else {
+    exit 0
+}
